70cbb29b76e3f0bb090b1d33090a0418d97df506
AptitudeAlchemy <aptitudealchemy@gmail.com>
2025-01-24 20:52:21 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import os
-
-username = os.getenv('username')
-log_path = f"C:\\Users\\{username}\\AppData\\Local\\AptitudeAlchemy\\"
-
-def write_log(message, type='info'):
-    with open(log_path+'log.txt', 'a+') as log_file:
-        log_file.write(f"[{type.upper()}] - {message} - {datetime.datetime.now().strftime('%Y:%m:%d %H:%M:%S')}\n")
